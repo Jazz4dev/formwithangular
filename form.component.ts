9ab97218f63f5df95e595e7a0b1ff2c9c5ac0848
Mohammed jaber <mhodjaber@gmail.com>
2020-07-26 23:25:50 -1200

    Update form.component.ts
    new iterms added

--- form.component.ts
+++ form.component.ts
@@ -6,13 +6,37 @@ import { Component,OnInit } from '@angular/core';
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent {
- 
+  show = true;
+  show1 =false;
+  show2 = false;
+    Next1(){
+    this.show1 =true ;
+    this.show = false;
+    this.progressValue = "50%"
+   }
+   preClick1(){
+     this.show1 = false;
+     this.show = true
+     this.progressValue ="16%"
+   }
+   Next2(){
+    this.show =false;
+    this.show1 =false;
+    this.show2 =true;
+    this.progressValue ="100%"
+   }
+   preClick2() {
+     this.show=false;
+     this.show1 =true;
+     this.show2 =false;
+     this.progressValue="50%"
+   }
+   previous0(){
+     alert('You Can not go Back')
+   }
   
   
      public progressValue : any = "17%";
-  
- 
-  
   trigger(tabType){
     if(tabType=='about'){
       this.progressValue="17%";
